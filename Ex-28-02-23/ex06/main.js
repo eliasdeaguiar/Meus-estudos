032a4470cc8d9f7d1a49afe43d8ee631d13228e5
Elias SIlva de Aguiar <109965221+eliasdeaguiar@users.noreply.github.com>
2023-03-17 15:03:51 -0300

    Melhorei um pouco mais o ex06

--- Ex-28-02-23/ex06/main.js
+++ Ex-28-02-23/ex06/main.js
@@ -4,6 +4,7 @@ const listaDeLinhas = document.querySelectorAll('.linhas_teclado');
 const listaDeTeclasNum = document.querySelectorAll('.teclas_num');
 const listaDeTeclasNum2 = document.querySelectorAll('.linhas_teclado_num');
 var outputId = '';
+var outputIdNaoSelecionado = '';
 
 const container = document.querySelector('#teclado');
 const containerTeclado = document.querySelector('#teclado01');
@@ -12,6 +13,7 @@ const containerNumerico = document.querySelector('#teclado02');
 //Verificação qual botão do teclado virtual foi clicado, transposição do valor teclado para o input selecionado e animação feita.
 document.addEventListener("click", function(event){
     if(event.target.closest(".output")){
+        outputIdNaoSelecionado = '';
         for(let contador = 0; contador < listaDeOutput.length; contador++){
             if(listaDeOutput[contador].id == event.target.id){
                 outputId = document.querySelector(`#${event.target.id}`);
@@ -19,20 +21,32 @@ document.addEventListener("click", function(event){
         }
     }
     if(event.target.closest(".teclas") && outputId != ''){
+        outputIdNaoSelecionado = outputId;
         for(let contador = 0; contador < listaDeTeclas.length; contador++){
             if(event.target.id == listaDeTeclas[contador].id){
-                teclaEfeito = listaDeTeclas[contador].id;
-                
                 listaDeTeclas[contador].classList.add('teclado');
                 setTimeout(function(){
                     listaDeTeclas[contador].classList.remove('teclado');
                 }, 500);
-                if(event.target.id == 'CapsLock' || event.target.id == 'ShiftLeft' || event.target.id == 'Space' || event.target.id == 'Backspace'){
+                if(event.target.id == 'CapsLock' || event.target.id == 'CapsLock2' || event.target.id == 'ShiftLeft' || event.target.id == 'ShiftLeft2' || event.target.id == 'Space' || event.target.id == 'Backspace'){
                     if(event.target.id == 'Backspace'){
                         let outputId2 = outputId.value;
                         outputId.value = outputId2.substring(0, outputId2.length -1);
+                        return;
                     }
-                    return;
+                    if(event.target.id == 'Space'){
+                        outputId.value += ' ';
+                    }
+                    if(event.target.id == 'CapsLock'){
+                        mostrarTeclado()
+                        
+                    }
+                    if(event.target.id == 'CapsLock2'){
+                        mostrarTeclado()
+                    }
+                    if(event.target.id == 'ShiftLeft'){
+                        mostrarTecladoNum()
+                    }
                 }else{
                     outputId.value += event.target.firstChild.nodeValue;
                 }
@@ -41,6 +55,7 @@ document.addEventListener("click", function(event){
         }
     }
     if(event.target.closest('.teclas_num') && outputId != ''){
+        outputIdNaoSelecionado = outputId;
         for(let contador = 0; contador < listaDeTeclasNum.length; contador++){
             if(event.target.id == listaDeTeclasNum[contador].id){
                 outputId.value += event.target.firstChild.nodeValue;
@@ -85,21 +100,6 @@ document.addEventListener("click", function(event){
     containerTeclado.style.display = 'none';
     containerNumerico.style.display = 'none';
 })
-
-document.addEventListener("keypress", function(event){
-    for(let contador = 0; contador < listaDeTeclas.length; contador++){
-        if(event.code == listaDeTeclas[contador].id){
-            // if(outputId != ''){
-            //     outputId.value += event.target.firstChild.nodeValue;
-            //     return;
-            // }
-            listaDeTeclas[contador].classList.add('teclado');
-            setTimeout(function(){
-            listaDeTeclas[contador].classList.remove('teclado');
-            }, 500);
-        }
-    }
-})
 let contadorParaNaoEntrarMaisDown = 0;
 let contadorParaNaoEntrarMaisUp = 0;
 
@@ -109,91 +109,83 @@ document.addEventListener('keyup', function(event){
         setTimeout(function(){
             document.querySelector('#CapsLock').classList.remove('teclado');
         }, 500);
-        for(let contador = 0; contador < listaDeLinhas.length; contador++){
-            if(listaDeLinhas[contador].classList.contains('mostrar_teclado')){
-                listaDeLinhas[contador].classList.add('esconder_teclado');
-                listaDeLinhas[contador].classList.remove('mostrar_teclado');
-            }else{
-                listaDeLinhas[contador].classList.remove('esconder_teclado');
-                listaDeLinhas[contador].classList.add('mostrar_teclado');
-            }
-        }
+        mostrarTeclado();
         return;
     }
     if(event.code == 'ShiftLeft'){
         if(contadorParaNaoEntrarMaisUp == 0){
             document.querySelector('#ShiftLeft').classList.remove('teclado');
             document.querySelector('#ShiftLeft2').classList.remove('teclado');
-            for(let contador = 0; contador < listaDeTeclasNum2.length; contador++){
-                if(listaDeTeclasNum2[contador].classList.contains('esconder_teclado')){
-                    listaDeTeclasNum2[contador].classList.add('mostrar_teclado');
-                    listaDeTeclasNum2[contador].classList.remove('esconder_teclado');
-                }else{
-                    listaDeTeclasNum2[contador].classList.remove('mostrar_teclado');
-                    listaDeTeclasNum2[contador].classList.add('esconder_teclado');
-                }
-            }
-            for(let contador = 0; contador < listaDeLinhas.length; contador++){
-                if(listaDeLinhas[contador].classList.contains('esconder_teclado')){
-                    listaDeLinhas[contador].classList.add('mostrar_teclado');
-                    listaDeLinhas[contador].classList.remove('esconder_teclado');
-                }else{
-                    listaDeLinhas[contador].classList.remove('mostrar_teclado');
-                    listaDeLinhas[contador].classList.add('esconder_teclado');
-                }
-            }
+            mostrarTecladoNum()
+            mostrarTeclado()
             contadorParaNaoEntrarMaisUp++;
             contadorParaNaoEntrarMaisDown = 0;
         }
     }
-
-    for(let contador = 0; contador < listaDeTeclasNum.length; contador++){
-        if(event.code == listaDeTeclasNum[contador].id){
-            listaDeTeclasNum[contador].classList.add('teclado');
-            setTimeout(function(){
-            listaDeTeclasNum[contador].classList.remove('teclado');
-            }, 500);
-        }
-    }
 })
 document.addEventListener('keydown', function(event){
+    let contadorParaLetras = 0;
     if(event.code == 'ShiftLeft'){
         if(contadorParaNaoEntrarMaisDown == 0){
             document.querySelector('#ShiftLeft').classList.add('teclado');
             document.querySelector('#ShiftLeft2').classList.add('teclado');
-            for(let contador = 0; contador < listaDeTeclasNum2.length; contador++){
-                if(listaDeTeclasNum2[contador].classList.contains('mostrar_teclado')){
-                    listaDeTeclasNum2[contador].classList.remove('mostrar_teclado');
-                    listaDeTeclasNum2[contador].classList.add('esconder_teclado');
-                }else{
-                    listaDeTeclasNum2[contador].classList.add('mostrar_teclado');
-                    listaDeTeclasNum2[contador].classList.remove('esconder_teclado');
-                }
-            }
-            for(let contador = 0; contador < listaDeLinhas.length; contador++){
-                if(listaDeLinhas[contador].classList.contains('mostrar_teclado')){
-                    listaDeLinhas[contador].classList.remove('mostrar_teclado');
-                    listaDeLinhas[contador].classList.add('esconder_teclado');
-                }else{
-                    listaDeLinhas[contador].classList.add('mostrar_teclado');
-                    listaDeLinhas[contador].classList.remove('esconder_teclado');
-                }
-            }
+            mostrarTecladoNum()
+            mostrarTeclado()
             contadorParaNaoEntrarMaisDown++;
             contadorParaNaoEntrarMaisUp = 0;
         }
     }
-    // if(event.key == 'Backspace'){
-    //     let tamanhoOutput = parseInt(outputId.value.length);
-    //     console.log(outputId.value[tamanhoOutput-2]);
-    //     if(outputId.value[tamanhoOutput-2] == '.' || outputId.value[tamanhoOutput-2] == '-'){
-    //         let outputId2 = outputId.value;
-    //         outputId.value = outputId2.substring(0, outputId2.length -2);
-    //     }else{
-    //         let outputId2 = outputId.value;
-    //         outputId.value = outputId2.substring(0, outputId2.length -1);
-    //     } 
-    // }
+    for(let contador = 0; contador < listaDeTeclas.length; contador++){
+        if(event.code == listaDeTeclas[contador].id){
+            if(contadorParaLetras == 0 && outputIdNaoSelecionado != ''){
+                if(event.code == 'CapsLock' || event.code == 'ShiftLeft' || event.code == 'Space' || event.code == 'Backspace'){
+                    if(event.code == 'Backspace'){
+                        let tamanhoOutput = parseInt(outputId.value.length);
+                        if(outputId.value[tamanhoOutput-2] == '.' || outputId.value[tamanhoOutput-2] == '-'){
+                            let outputId2 = outputId.value;
+                            outputId.value = outputId2.substring(0, outputId2.length -2);
+                        }else{
+                            let outputId2 = outputId.value;
+                            outputId.value = outputId2.substring(0, outputId2.length -1);
+                        }
+                    }
+                    // if(event.code == 'Space'){
+                    //     outputId.value.replace(/\w/g, ' ');
+                    // }
+                }else{
+                    outputId.value += event.key;
+                }
+            }
+            listaDeTeclas[contador].classList.add('teclado');
+            setTimeout(function(){
+            listaDeTeclas[contador].classList.remove('teclado');
+            }, 500);
+            contadorParaLetras++;
+        }  
+    }
+
+    for(let contador = 0; contador < listaDeTeclasNum.length; contador++){
+        if(event.code == listaDeTeclasNum[contador].id){
+            if(event.code == listaDeTeclasNum[contador].id){
+                if(contadorParaLetras == 0 && outputIdNaoSelecionado != ''){
+                    if(outputId.id == 'cpf'){
+                        let x = outputId.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})/);
+                        outputId.value = !x[2] ? x[1] : x[1] + '.' + x[2] + (x[3] ? '.' + x[3] : '') + (x[4] ? '-' + x[4] : '');
+                    }
+                    if(outputId.id == 'data-nascimento'){
+                        let x = outputId.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,2})(\d{0,4})/);
+                        outputId.value = !x[2] ? x[1] : x[1] + '/' + x[2] + (x[3] ? '/' + x[3] : '');
+                    }
+                    outputId.value += event.key;
+                }
+            }  
+            listaDeTeclasNum[contador].classList.add('teclado');
+            setTimeout(function(){
+                listaDeTeclasNum[contador].classList.remove('teclado');
+            }, 500);
+            contadorParaLetras++;
+        }
+    }
     if(event.code == 'CapsLock'){
         document.querySelector('#CapsLock').classList.add('teclado');
         setTimeout(function(){
@@ -204,11 +196,34 @@ document.addEventListener('keydown', function(event){
 
 //Mascaras para cpf e data de nascimento
 document.getElementById('cpf').addEventListener('input', function (event) {
-    let x = event.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})/);
-    event.target.value = !x[2] ? x[1] : x[1] + '.' + x[2] + (x[3] ? '.' + x[3] : '') + (x[4] ? '-' + x[4] : '');
+    let campo = event.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,2})/);
+    event.target.value = !campo[2] ? campo[1] : campo[1] + '.' + campo[2] + (campo[3] ? '.' + campo[3] : '') + (campo[4] ? '-' + campo[4] : '');
 });
 
 document.getElementById('data-nascimento').addEventListener('input', function (event) {
-    let x = event.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,2})(\d{0,4})/);
-    event.target.value = !x[2] ? x[1] : x[1] + '/' + x[2] + (x[3] ? '/' + x[3] : '');
+    let campo = event.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,2})(\d{0,4})/);
+    event.target.value = !campo[2] ? campo[1] : campo[1] + '/' + campo[2] + (campo[3] ? '/' + campo[3] : '');
 });
+
+function mostrarTeclado(){
+    for(let contador = 0; contador < listaDeLinhas.length; contador++){
+        if(listaDeLinhas[contador].classList.contains('mostrar_teclado')){
+            listaDeLinhas[contador].classList.add('esconder_teclado');
+            listaDeLinhas[contador].classList.remove('mostrar_teclado');
+        }else{
+            listaDeLinhas[contador].classList.remove('esconder_teclado');
+            listaDeLinhas[contador].classList.add('mostrar_teclado');
+        }
+    }
+}
+function mostrarTecladoNum(){
+    for(let contador = 0; contador < listaDeTeclasNum2.length; contador++){
+        if(listaDeTeclasNum2[contador].classList.contains('mostrar_teclado')){
+            listaDeTeclasNum2[contador].classList.remove('mostrar_teclado');
+            listaDeTeclasNum2[contador].classList.add('esconder_teclado');
+        }else{
+            listaDeTeclasNum2[contador].classList.add('mostrar_teclado');
+            listaDeTeclasNum2[contador].classList.remove('esconder_teclado');
+        }
+    }
+}
