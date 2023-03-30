574c2491e075c0e7cd8054f8fa3f802d08bedd99
Elias SIlva de Aguiar <109965221+eliasdeaguiar@users.noreply.github.com>
2023-03-30 14:58:04 -0300

    Mais estudos feitos sobre ux

--- aulas-por-conta-propria/Estudos de UX e UI/js/main.js
+++ aulas-por-conta-propria/Estudos de UX e UI/js/main.js
@@ -1,11 +1,7 @@
 const listaDesignThinking = document.querySelectorAll('.lista_design_thinking');
 var etapaClicada = ''
 window.addEventListener('click', function(event){
-    for(let contador = 0; contador < listaDesignThinking.length; contador++){
-        if(event.target.closest(`#lista_design_thinking_${contador}`)){
-            etapaClicada = event.originalTarget.id;
-        }
-    }
+    etapaClicada = event.originalTarget.id;
     for(let contador = 0; contador < listaDesignThinking.length; contador++){
         if(etapaClicada == listaDesignThinking[contador].parentElement.id){
             if(listaDesignThinking[contador].classList.contains('aparecer')){
